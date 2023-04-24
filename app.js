68a6e59867ae297cb489842ab1eac4debca6ff98
ZeitLee <li.zes@husky.neu.edu>
2023-04-23 22:20:12 -0400

    update db on cloud

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import ReviewController from "./reviews/reviews-controller.js";
 import FollowController from "./follows/follows-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017/web-users');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/web-users';
+
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
